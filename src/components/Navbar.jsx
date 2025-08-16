3ee20fffbc34d48e051b1b6775356c1d0ff3aea1
Haider-khangit551 <hayderkhan24122004@gmail.com>
2025-08-16 14:11:40 +0530

    Update Navbar.jsx
    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -101,11 +101,12 @@ const Navbar = ({ theme, setTheme }) => {
     }
 
     return (
-        <motion.div
+         <motion.div
             initial={{ opacity: 0, y: -50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, ease: 'easeOut' }}
-            className='flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70'>
+            className='flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 fixed top-0 left-0 right-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70'
+        >
 
             {
                 theme === "dark" ?
